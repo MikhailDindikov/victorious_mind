64c7959389e7639d85389c4b8e8fca1c8ac711a9
MihailDundukov <misha2001du@gmail.com>
2024-05-28 18:16:03 +0300

    2

--- victorious_mind/lib/gase/gaga/mode_cardd.dart
+++ victorious_mind/lib/gase/gaga/mode_cardd.dart
@@ -6,12 +6,16 @@ class ModeCardd {
   late ui.Image kartaCard;
   late String chtoTakCard;
 
-  int isDiffCard(ModeCardd c1, ModeCardd c2) {
+  bool isDiffCard(ModeCardd c1, ModeCardd c2) {
     if (c1.poRowCard == c2.poRowCard && c1.poColCard == c2.poColCard) {
       if (c1.chtoTakCard == c2.chtoTakCard && c1.kartaCard == c2.kartaCard) {
-        return 1;
+        return false;
       }
     }
-    return 0;
+    return true;
+  }
+
+  int sumCard(ModeCardd c1, ModeCardd c2) {
+    return c1.poRowCard + c2.poRowCard;
   }
 }
